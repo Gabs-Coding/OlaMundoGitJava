616a419df663e9d4ff6e55dcfe2e6ba5b6a84535
Gabs <gsbiel62@gmail.com>
2023-09-21 11:24:30 -0400

    Adicionado OlaMundo em Java
    Um simples olá mundo escrito em Java e somando dois números. Fiz só para ver o Git funcionando.

---
+++ olamundogit/src/main/java/gabscoding/Main.java
@@ -0,0 +1,15 @@
+package gabscoding;
+
+public class Main {
+
+    public static Integer somarInteiros(Integer a, Integer b) {
+        return a+b;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world! The sum of 1 and 2 is " + somarInteiros(1, 2));
+    }
+}
